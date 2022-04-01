b89b0a64dcea0b7df308f937680344e7bedfadf5
Xiaopeng1027 <101281565+Xiaopeng1027@users.noreply.github.com>
2022-04-01 19:42:24 +0800

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	cout << "hi ,i'm@Xiaopeng1027" << endl;
+	cout << "哈哈哈，这是我在github的第一个文件" << endl;
+	cout << "你好" << endl;
+	system("pause");
+}
